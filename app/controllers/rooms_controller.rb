08999284be2f64ee6ca633cba82f18d87ea13670
TMY-GH <prog0323fujii@gmail.com>
2021-06-17 20:42:11 +0900

    新規作成画面の表示

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,16 @@
+class RoomsController < ApplicationController
+  
+  def new
+    @room = Room.new
+  end
+
+  def create
+    Room.create(room_params)
+  end
+
+  private
+  def room_params
+    params.require(:room).permit(:name)
+  end
+
+end
